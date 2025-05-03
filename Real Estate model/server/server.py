3e6db469a722bf437055573b74f23804c9302b42
PratyushPoddar07 <shuvampoddar018@gmail.com>
2025-05-03 23:00:24 +0530

    backend

--- Real Estate model/server/server.py
+++ Real Estate model/server/server.py
@@ -1,30 +1,54 @@
-# flask can solve http request as a python service
 from flask import Flask, request, jsonify
 import util
+
 app = Flask(__name__)
 
-@app.route('/get_location_names')
+
+@app.route('/get_location_names', methods=['GET'])
 def get_location_names():
     response = jsonify({
-        'locations':util.get_location_names()
+        'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+
 
 @app.route('/predict_home_price', methods=['POST'])
 def predict_home_price():
-    total_sqft = float(request.form['total_sqft'])
-    location = request.form['location']
-    bhk = int(request.form['bhk'])
-    bath = int(request.form['bath'])
+    try:
+        # Handle both form data and JSON
+        if request.content_type == 'application/json':
+            data = request.get_json()
+            total_sqft = float(data['total_sqft'])
+            location = data['location']
+            bhk = int(data['bhk'])
+            bath = int(data['bath'])
+        else:
+            # Form data
+            total_sqft = float(request.form['total_sqft'])
+            location = request.form['location']
+            bhk = int(request.form['bhk'])
+            bath = int(request.form['bath'])
 
-    response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        response = jsonify({
+            'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
+        })
+        response.headers.add('Access-Control-Allow-Origin', '*')
+        return response
 
-    })
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    except Exception as e:
+        return jsonify({
+            'error': str(e)
+        }), 400  # Bad Request
 
-if __name__ =="__main__":
-    print("Starting python Flask server for home price prediction...")
-    app.run()
+
+@app.route('/test', methods=['GET'])
+def test():
+    """Simple test endpoint to verify server is running"""
+    return jsonify({'status': 'Server is up and running!'})
+
+
+if __name__ == "__main__":
+    print("Starting Python Flask server for home price prediction...")
+    util.load_saved_artifacts()
+    app.run(debug=True)  # Enable debug mode for better error messages
--- Real Estate model/server/util.py
+++ Real Estate model/server/util.py
@@ -2,23 +2,23 @@ import json
 import pickle
 import numpy as np
 
-__location =None
+__locations =None
 __data_columns = None
 __model = None
 
-def get_location_names():
-    return __location
+
 
 def get_estimated_price(location,sqft,bhk,bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
         loc_index = -1
+
     x = np.zeros(len(__data_columns))
     x[0] =sqft
     x[1] =bath
     x[2] =bhk
-    if loc_index >= 0 :
+    if loc_index >= 0:
         x[loc_index] = 1
     return round(__model.predict([x])[0],2)
 
@@ -27,17 +27,23 @@ def get_estimated_price(location,sqft,bhk,bath):
 def load_saved_artifacts():
     print("Loading saved artifacts...start")
     global __data_columns
-    global __location
+    global __locations
 
     with open("./artifacts/columns.json",'r') as f:
         __data_columns = json.load(f)['data_columns']
-        __location = __data_columns[3:]
+        __locations = __data_columns[3:]
 
     global __model
-    with open("./artifacts/banglore_home_price_model.pickle",'rb') as f:
-        __model =pickle.load(f)
+    if __model is None:
+        with open("./artifacts/bangalore_home_price_model.pickle",'rb') as f:
+            __model =pickle.load(f)
     print("Loading saved artifacts...done")
 
+def get_data_columns():
+    return __data_columns
+
+def get_location_names():
+    return __locations
 
 if __name__ == "__main__":
     load_saved_artifacts()
